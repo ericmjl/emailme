9384d25bd73b93a3f94059fe7623bccf14dffdba
Eric Ma <ericmajinglong@gmail.com>
2018-04-21 12:46:19 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(fname):
 
 
 setup(name="emailme",
-      version="0.1.1",
+      version="0.1.2",
       author="Eric J. Ma",
       author_email="ericmajinglong@gmail.com",
       description=("Super simple self-emailing."),
